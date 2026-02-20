457f499bec7aaa13481f22c62c33c8e3ed0fe365
iceship <iceship@gmail.com>
2026-02-20 21:28:23 +0900

    chore: update compatibility date and enhance configuration in nuxt.config.ts; add .npmrc for package management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-12-11',
   devtools: { enabled: true },
-  modules: ['@nuxthub/core']
+  modules: [
+    '@nuxthub/core',
+    '@nuxt/eslint',
+  ],
+  hub: {
+    cache: true
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
